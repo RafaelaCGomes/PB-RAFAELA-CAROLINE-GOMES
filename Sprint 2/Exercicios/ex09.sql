3c6ef9e61896a08598a998139e699d9b44464bad
Rafaela Caroline Gomes <rafa.carolyne.gomes@gmail.com>
2024-11-05 14:06:04 -0300

    Exercicios

--- Sprint 2/Exercicios/ex09.sql
+++ Sprint 2/Exercicios/ex09.sql
@@ -2,6 +2,14 @@
 entre as datas de 2014-02-03 até 2018-02-02, e que estas vendas estejam com o status concluída.
 As colunas presentes no resultado devem ser cdpro e nmpro.*/
 
+SELECT cdpro, nmpro
+FROM tbvendas t 
+WHERE dtven BETWEEN '2014-02-03' AND '2018-02-02'
+	AND status = 'Concluído'
+GROUP BY cdpro, nmpro 
+ORDER BY COUNT(cdpro) DESC
+limit 1;
+
 
 
 
