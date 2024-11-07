67662968f0399f466be25ca23a06720d8fac0f2a
Rafaela Caroline Gomes <rafa.carolyne.gomes@gmail.com>
2024-11-07 15:00:24 -0300

    Desafio

--- Sprint 2/Desafio/README.md
+++ Sprint 2/Desafio/README.md
@@ -17,28 +17,21 @@ Analisando a tabela e as colunas, achei melhor fazer uma modelagem pelo Dbdesign
 
 Tive então um rascunho inicial para organizar o pensamento: [Rascunho_modelagem](../Evidencias/Modelagem_inicial.jpg)
 
-2. Em seguida, dei inicio a criação das tabelas e a normalização dos dados, fazendo as relações entre as tabelas.
+2. Em seguida, dei inicio a criação das tabelas e a normalização dos dados, pensei em ja fazer a relaçõa entre as tabelas novas e a tb_locação.
 
 Nesse momento tive muita dificulade com a sintax e quais comandos usar, achei necessário realizar uma busca em outros materiais para conseguir entender melhor como fazer.
 
-3. Após pesquisar em outros cursos da Udemy e por fora da plataforma, comecei criando a tabela vendedor e adicionando as colunas correspondentes a essa tabela, inseri os idVendedor, porém, quando fui inserir a chhave estrangeira na tb_locacao, apareceu um erro.
+3. Após pesquisar em outros cursos da Udemy e por fora da plataforma, comecei criando a tabela vendedor e adicionando as colunas correspondentes a essa tabela, porém, quando fui inserir a chhave estrangeira na tb_locacao, apareceu um erro.
 
 ![Erro_SQL](../Evidencias/Erro_sql.jpg)
 
-❌[Query erro](../Desafio/etapa-1/Erro_foreign_key.sql)
+Após conversar e pesquisar sobre o erro,  entendi que essa tabela tb_locacao não seria possível alterar, então, comecei criando novas tabelas para normalizar todos os dados.
 
-Após conversar e pesquisar sobre o erro, achei melhor continuar criando as outras tabelas, para depois criar novamente a tabela locação e então fazer as conexões entre tabelas.
-
-4. Continuei criando as tabelas e normalizar os dados.
-
-[Tabela Vendedor](../Desafio/etapa-1/Tb_vendas.sql)
-![Tabela vendedor](../Evidencias/Criação_vendedor.jpg)
+4. Criei primeiro todas as tabelas necessárias, para então ir transferindo os dados da Antiga_tb_locacao.
 
 
-[Criação demais tabelas (cliente, carro e combustivel)](../Desafio/etapa-1/Criacao_tabelas.sql)
-![Tabela Carro](../Evidencias/Criação_tabelas.jpg)
 
-5. Achei melhor ja criar também uma segunda tabela locação, para que eu possa adicionar as chaves estrangeiras e fazer as alterações necessárias nas colunas.
+
 
 
 
---
+++ Sprint 2/Desafio/etapa-1/Etapa01_Relacional.sql
@@ -0,0 +1,166 @@
+--Criação da tabela vendedor.
+CREATE TABLE Vendedor(
+	idVendedor INT NOT NULL PRIMARY KEY,
+	nomeVendedor VARCHAR (100),
+	sexoVendedor SMALLINT (10),
+	estadoVendedor VARCHAR (100)
+);
+--Criação da tabela cliente.
+CREATE TABLE Cliente (
+	idCliente INT PRIMARY KEY,
+	nomeCliente VARCHAR (100),
+	cidadeCliente VARCHAR (100),
+	estadoCliente VARCHAR (100),
+	paisCliente VARCHAR (100)
+);
+
+--Criação da tabela combustivel.
+CREATE TABLE Combustivel (
+	idCombustivel INT PRIMARY KEY,
+	tipoCombustivel INT
+);
+
+--Criação da tabela carro.
+CREATE TABLE Carro (
+	idCarro INT PRIMARY KEY,
+	kmCarro INT,
+	classiCarro VARCHAR (100),
+	marcaCarro VARCHAR (100),
+	modeloCarro VARCHAR (100),
+	anoCarro INT,
+	idCombustivel INT,
+	FOREIGN KEY (idCombustivel) REFERENCES Combustivel(idCombustivel)
+);
+
+
+--Alteração do nome da tabela antiga.
+ALTER TABLE tb_locacao RENAME TO Antiga_tb_locacao;
+
+--Criação da tabela locacao.
+CREATE TABLE Locacao (
+	idLocacao INT PRIMARY KEY,
+	dataLocacao DATE,
+	horaLocacao TIME,
+	qtdDiaria INT,
+	vlrDiaria DECIMAL (5,2),
+	dataEntrega DATE,
+	horaEntrega TIME,
+	idCliente INT,
+	idVendedor INT,
+	idCarro INT,
+	FOREIGN KEY (idCliente) REFERENCES Cliente(idCliente),
+	FOREIGN KEY (idVendedor) REFERENCES Vendedor(idVendedor),
+	FOREIGN KEY (idCarro) REFERENCES Carro(idCarro)
+);
+
+--Dados na tabela vendedor.
+INSERT INTO Vendedor (idVendedor,nomeVendedor, sexoVendedor, estadoVendedor)
+SELECT DISTINCT idVendedor,nomeVendedor, sexoVendedor, estadoVendedor
+FROM Antiga_tb_locacao ;
+
+SELECT *
+FROM Vendedor v ;
+
+--Dados na tabela cliente.
+INSERT INTO Cliente (idCliente, nomeCliente, CidadeCliente, estadoCliente, paisCliente)
+SELECT DISTINCT idCliente, nomeCliente, cidadeCliente, estadoCliente, paisCliente
+FROM Antiga_tb_locacao ;
+
+SELECT *
+FROM Cliente c ;
+	
+--Dados na tabela combustivel.
+INSERT INTO Combustivel (idCombustivel, tipoCombustivel)
+SELECT DISTINCT idcombustivel , tipocombustivel
+FROM Antiga_tb_locacao ;
+
+SELECT *
+FROM Combustivel c ;
+
+--Dados na tabela carro.
+INSERT INTO Carro (idCarro, kmCarro, classiCarro, marcaCarro, modeloCarro, anoCarro, idCombustivel )
+SELECT DISTINCT idCarro, kmCarro, classiCarro, marcaCarro, modeloCarro, anoCarro, idCombustivel 
+FROM Antiga_tb_locacao 
+WHERE kmCarro = (
+	SELECT MAX(kmCarro)
+	FROM Antiga_tb_locacao AS kmMax
+	WHERE kmMax.idCarro = Antiga_tb_locacao.idCarro
+) GROUP BY idCarro;
+
+SELECT *
+FROM Carro c ;
+
+--Dados na tabela Locação.
+INSERT INTO Locacao (idLocacao, dataLocacao, horaLocacao, qtdDiaria, vlrDiaria, dataEntrega, horaEntrega, idCliente, idVendedor, idCarro)
+SELECT DISTINCT idLocacao, dataLocacao, horaLocacao, qtdDiaria, vlrDiaria, dataEntrega, horaEntrega, idCliente, idVendedor, idCarro
+FROM Antiga_tb_locacao ;
+
+
+SELECT *
+FROM Locacao l ;
+
+--Formatação da coluna horaLocacao.
+UPDATE Locacao 
+SET horaLocacao = printf('%02d:%s', CAST(substr(horaLocacao , 1, instr(horaLocacao , ':') - 1) AS INTEGER), substr(horaLocacao , instr(horaLocacao , ':') + 1));
+
+--Formatação da coluna dataLocação para YYYY/MM/DD.
+UPDATE Locacao 
+SET dataLocacao = substr(dataLocacao, 1, 4) || '-' || substr(dataLocacao, 5, 2) || '-' || substr(dataLocacao , 7, 2);
+
+--Formatação da coluna dataEntrega para YYYY/MM/DD.
+UPDATE Locacao 
+SET dataEntrega = substr(dataEntrega , 1, 4) || '-' || substr(dataEntrega , 5, 2) || '-' || substr(dataEntrega , 7, 2);
+
+SELECT *
+FROM Locacao l ;
+
+
+--Deletando a antiga tabela locação.
+
+DROP TABLE Antiga_tb_locacao ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
